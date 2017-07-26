d4be5b9d56504d0b51c5068302eca5bf323d1afb
JJ Merelo <jjmerelo@gmail.com>
2017-07-26 08:39:08 +0200

    Evolució de las notas de corte

---
+++ notas-corte.R
@@ -0,0 +1,11 @@
+require("ggplot2")
+require("ggthemes")
+
+notas <- read.csv("notas_corte_hasta_2016.csv",sep=",")
+notas$Año <- as.numeric(notas$Año)
+notas$Nota.general <- as.numeric(notas$Nota.general)
+
+ggplot(notas)+geom_line(aes(x=Año,y=Nota.general,group=interaction(Grado.en,Centro,Universidad),color=Universidad))+geom_point(aes(x=Año,y=Nota.general,color=Universidad,shape=Rama))
+
+notas.granada <- notas[notas$Universidad=="GRANADA",]
+ggplot(notas.granada)+geom_line(aes(x=Año,y=Nota.general,group=interaction(Grado.en,Centro),color=Centro))+geom_point(aes(x=Año,y=Nota.general,color=Centro,shape=Rama))
